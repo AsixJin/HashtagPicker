681ce997ff0a4afb301ffd16deceb7d96dc2e4f3
Jeff Willis <jwillis@myocv.com>
2018-05-31 14:18:06 -0500

    Commented test file & Updated project

--- test/file_test.dart
+++ test/file_test.dart
@@ -1,16 +1,6 @@
-// This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
-
-import 'package:flutter/material.dart';
+//This test allows any file writing/reading to work when testing
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hashtag_picker/main.dart';
-import 'dart:io';
 import 'package:flutter/services.dart';
-
 
 void main() {
   setUpAll(() async {
