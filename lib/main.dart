d607267a5c9b4b7c7fc6e6ab30fd2b5f227e9e11
Jeff Willis <jwillis@myocv.com>
2018-05-31 11:44:45 -0500

    Added ability to save hashtags

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:math';
@@ -10,10 +17,11 @@ final listKey = new GlobalKey<ScaffoldState>();
 //region  Hashtag Methods
 List<String> hashtags = new List<String>();
 
-void addHashtag(TextEditingController controller) {
+bool addHashtag(TextEditingController controller) {
   String toastMsg = "";
   String tag = controller.text.toLowerCase();
   controller.text = "";
+  bool isSuccess = false;
 
   if (tag.contains("#")) {
     tag = tag.replaceAll("#", "");
@@ -30,11 +38,13 @@ void addHashtag(TextEditingController controller) {
   } else {
     toastMsg = "#" + tag + " added!";
     hashtags.add(tag);
+    isSuccess = true;
   }
 
   generatorKey.currentState.showSnackBar(new SnackBar(
     content: new Text(toastMsg),
   ));
+  return isSuccess;
 }
 
 void deleteHashtag(String tag) {
@@ -42,6 +52,44 @@ void deleteHashtag(String tag) {
   listKey.currentState.showSnackBar(new SnackBar(
     content: new Text("#" + tag + " removed!"),
   ));
+}
+//endregion
+
+//region Saving/Loading
+class ListStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return new File('$path/hashtags.txt');
+  }
+
+  Future<File> writeList(List<String> list) async {
+    final file = await _localFile;
+
+    String feed = json.encode(list);
+
+    // Write the file
+    return file.writeAsString(feed);
+  }
+
+  Future<List> readList() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      String contents = await file.readAsString();
+      List<String> list = json.decode(contents);
+
+      return list;
+    } catch (e) {
+      // If we encounter an error, return 0
+      return new List();
+    }
+  }
 }
 //endregion
 
@@ -53,6 +101,10 @@ int next(int min, int max) => min + _random.nextInt(max - min);
 
 //region Hash Generator
 class HashGenerator extends StatefulWidget {
+  final ListStorage storage;
+
+  HashGenerator({Key key, @required this.storage}) : super(key: key);
+
   @override
   _HashGeneratorState createState() => new _HashGeneratorState();
 }
@@ -62,6 +114,14 @@ class _HashGeneratorState extends State<HashGenerator> {
   Text hashtagText = new Text("");
   TextEditingController controller = new TextEditingController();
   TextEditingController controller2 = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readList().then((List value) {
+        hashtags = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +138,9 @@ class _HashGeneratorState extends State<HashGenerator> {
           new Align(
             child: new Row(children: <Widget>[
               buildButton(
-                  "Add", buttonPadding, true, () => addHashtag(controller)),
+                  "Add", buttonPadding, true, (){if(addHashtag(controller)){
+                return widget.storage.writeList(hashtags);
+              }}),
               buildButton("List", buttonPadding, true, _pushSaved)
             ]),
             alignment: Alignment.centerRight,
@@ -147,13 +209,17 @@ class _HashGeneratorState extends State<HashGenerator> {
   void _pushSaved() {
     Navigator
         .of(context)
-        .push(new MaterialPageRoute(builder: (context) => new HashList()));
+        .push(new MaterialPageRoute(builder: (context) => new HashList(storage: new ListStorage())));
   }
 }
 //endregion
 
 //region Hash List
 class HashList extends StatefulWidget {
+  final ListStorage storage;
+
+  HashList({Key key, @required this.storage}) : super(key: key);
+
   @override
   _HashListState createState() => new _HashListState();
 }
@@ -168,7 +234,8 @@ class _HashListState extends State<HashList> {
             "#" + tag,
           ),
           onTap: () {
-            setState(() => deleteHashtag(tag));
+            setState((){deleteHashtag(tag);});
+            return widget.storage.writeList(hashtags);
           },
         );
       },
@@ -223,7 +290,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: new HashGenerator(),
+      home: new HashGenerator(storage: new ListStorage()),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,22 +8,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hashtag_picker/main.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+  setUpAll(() async {
+    // Create a temporary directory to work with
+    final directory = await Directory.systemTemp.createTemp();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Mock out the MethodChannel for the path_provider plugin
+    const MethodChannel('plugins.flutter.io/path_provider')
+        .setMockMethodCallHandler((MethodCall methodCall) async {
+      // If we're getting the apps documents directory, return the path to the
+      // temp directory on our test environment instead.
+      if (methodCall.method == 'getApplicationDocumentsDirectory') {
+        return directory.path;
+      }
+      return null;
+    });
   });
+
 }
+
+
